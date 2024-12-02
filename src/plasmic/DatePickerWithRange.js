29c668d240c7f33be05d595626c6a99ac28726ec
Ali raza <malirazaansari4@gmail.com>
2024-12-02 12:14:06 +0500

    plasmic done hope so

---
+++ src/plasmic/DatePickerWithRange.js
@@ -0,0 +1,7 @@
+import { DatePickerWithRange } from "@/components/DateRange";
+import React from "react";
+
+// Register your component with Plasmic
+export const DatePickerWithRangePlasmic = (props) => {
+  return <DatePickerWithRange {...props} />;
+};
